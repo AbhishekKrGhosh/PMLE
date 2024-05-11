11e13e996bb56d58d35934535e476dcfb3b1f438
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-05-11 13:08:23 +0530

    Completed Basic layouting

--- src/Layouts/Drawer/SideBar.jsx
+++ src/Layouts/Drawer/SideBar.jsx
@@ -3,7 +3,10 @@
 function SideBar (){
 
     return (
-        <div className="w-24 h-screen bg-slate-500"></div>
+        <div className="w-24 h-screen bg-slate-500">
+        {/* Side Bar Content Goes Here */}
+            
+        </div>
     )
 }
 export default SideBar
--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -4,7 +4,7 @@ function Header (){
     return (
         <div>
              <div className="w-full h-20 bg-blue-300">
-                
+                {/* Header Content goes Here */}
              </div>
         </div>
     )
