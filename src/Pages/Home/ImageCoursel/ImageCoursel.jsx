9eafe89a1054412da58e96767caf2623af68321a
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-05-12 10:03:14 +0530

    Fixes the image Coursel

--- src/Pages/Home/ImageCoursel/ImageCoursel.jsx
+++ src/Pages/Home/ImageCoursel/ImageCoursel.jsx
@@ -9,19 +9,19 @@ function ImageCoursel (){
     return (
       
         <div className="embla w-full h-[500px]  " ref={emblaRef} >
-        <div className="embla__container ">
+        <div className="embla__container object-cover w-[100%] h-[100%] border-2 border-gray-600   ">
 
-          <div className="embla__slide  flex justify-center items-center" >
-            <img className='w-[100%] h-[100%] object-cover'  src='https://images.velog.io/images/saurabhdev/post/0c1174a8-06f2-49d8-b3bf-d9502c78a88e/Machine-Learning-course-in-Chennai.png'></img>
-          </div>
+         
+            <img className='embla__slide' src='https://images.velog.io/images/saurabhdev/post/0c1174a8-06f2-49d8-b3bf-d9502c78a88e/Machine-Learning-course-in-Chennai.png'></img>
+      
 
-          <div className="embla__slide flex justify-center items-center w-full">
-          <img className='w-[100%] h-[100%] object-cover' src='https://t3.ftcdn.net/jpg/05/14/95/12/360_F_514951224_2dxMLbIw5qNRdPGD003chpbVcxWtcp7K.jpg'></img>
-          </div>
+       
+          <img className='embla__slide' src='https://t3.ftcdn.net/jpg/05/14/95/12/360_F_514951224_2dxMLbIw5qNRdPGD003chpbVcxWtcp7K.jpg'></img>
+         
 
-          <div className="embla__slide flex justify-center items-center">
-          <img className='w-[100%] h-[100%] object-cover' src='https://images.pexels.com/photos/573130/pexels-photo-573130.jpeg?cs=srgb&dl=pexels-zyuliansyah-573130.jpg&fm=jpg'></img>
-          </div>
+         
+          <img className='embla__slide' src='https://images.pexels.com/photos/573130/pexels-photo-573130.jpeg?cs=srgb&dl=pexels-zyuliansyah-573130.jpg&fm=jpg'></img>
+        
 
         </div>
       </div>
